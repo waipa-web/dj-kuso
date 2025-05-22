4ee42de5625cc086e49be940d3228e596e1e8729
waipa-web <157952025+waipa-web@users.noreply.github.com>
2025-05-22 15:45:02 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -4,12 +4,12 @@ useSeoMeta({
 	ogTitle: "Rent Kuso",
 	description: "Turn any event into a party! Rent professional DJs with top-tier sound equipment for weddings, birthdays, corporate events, and more. Book now and bring the beats to your celebration.",
 	ogDescription: "Turn any event into a party! Rent professional DJs with top-tier sound equipment for weddings, birthdays, corporate events, and more. Book now and bring the beats to your celebration.",
-	ogImage: "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/aethereal.my.id/%C3%A6thereal/https%3A%2F%2Faethereal.my.id%2Flogo.jpg/og.png",
+	ogImage: "https://www.rentalkuso.web.id/icon.png",
 	twitterCard: "summary_large_image",
 	ogimageAlt: "Rental Kuso",
 	ogImageWidth: "226",
 	ogImageHeight: "226",
-	twitterImage: "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/aethereal.my.id/%C3%A6thereal/https%3A%2F%2Faethereal.my.id%2Flogo.jpg/og.png",
+	twitterImage: "https://www.rentalkuso.web.id/icon.png",
 });
 </script>
 
