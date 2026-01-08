3fd259cad461063fabedb061c049e859aaf14f8a
waipa-web <157952025+waipa-web@users.noreply.github.com>
2026-01-08 20:46:40 +0700

    index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,9 @@
 							<CardTitle>{{ product.name }}</CardTitle>
 						</CardHeader>
 
+						<section class="product pt-50">
+		<h1 class="text-center font-bold text-2xl">Our Product</h1>
+
 						<CardContent class="flex flex-col items-center mt-5">
 							<img :src="product.image" alt="product image" class="w-full h-48 object-cover mb-2" />
 						</CardContent>
